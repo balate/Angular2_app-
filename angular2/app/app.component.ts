9089a285291a60a5de48ae6eef13ce0837d19be7
Javier Contreras <javi_ocon@yahoo.es>
2016-08-14 13:49:40 +0200

    se crea primer commit. Incluye 1Archivos package,tsconfig, index  2 como crer primer component, ejecutar

---
+++ angular2/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: "my-app",
+    template:   `<h1>Hola Mundo</h1>
+    <p>otro texto</p>
+    <p>otro texo mas...</p>`
+})
+
+export class AppComponent{
+
+}
